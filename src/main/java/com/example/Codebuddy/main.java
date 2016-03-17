805a88e8f0109160ed3764d9126d8390633c322f
Joost de Ruijter <joost1235@hotmail.com>
2016-03-17 14:30:23 +0100

    memes

--- src/main/java/com/example/Codebuddy/main.java
+++ src/main/java/com/example/Codebuddy/main.java
@@ -2,6 +2,6 @@ package com.example.Codebuddy;
 
 public class main {
     public static void main(String[] args) {
-        System.out.println("Hello boys");
+        System.out.println("Hello memes");
     }
 }
