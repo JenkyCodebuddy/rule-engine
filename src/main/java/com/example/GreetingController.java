ed2243886f5cdc7ed3a6ce4964b391810dfd9bb2
Joost de Ruijter <joost1235@hotmail.com>
2016-04-26 10:02:00 +0200

    scocalc test

--- src/main/java/com/example/GreetingController.java
+++ src/main/java/com/example/GreetingController.java
@@ -13,7 +13,7 @@ public class GreetingController {
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
-
+    //test
 
     @RequestMapping("/greeting")
     public GreetingModel greeting(@RequestParam(value="name", defaultValue="World") String name) {
