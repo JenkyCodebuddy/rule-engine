aeb8230759c88cbd33ca8eb09072a56d5401f3b5
Joost de Ruijter <joost1235@hotmail.com>
2016-05-13 16:48:05 +0200

    changed test

--- src/test/java/jenky/codebuddy/RestClientTest.java
+++ src/test/java/jenky/codebuddy/RestClientTest.java
@@ -7,24 +7,6 @@ import java.io.IOException;
 import java.net.HttpURLConnection;
 
 public class RestClientTest {
-    private RestClient restclient;
-
-    public RestClientTest(){
-        restclient = new RestClient("admin","admin");
-    }
-
-    @Test
-    public void testURL(){
-        String strUrl = "/sonar/api/";
-
-        try {
-           restclient.get(strUrl);
-
-        } catch (Exception e) {
-            System.err.println("Error creating HTTP connection");
-            e.printStackTrace();
-            throw e;
-        }
-    }
+    
 
 }
