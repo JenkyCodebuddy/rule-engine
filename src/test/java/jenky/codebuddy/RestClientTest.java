0906be438879f262aaa9b954024f935abfe9cdf0
Joost de Ruijter <joost1235@hotmail.com>
2016-05-23 13:11:05 +0200

    added sample test class

--- src/test/java/jenky/codebuddy/RestClientTest.java
+++ src/test/java/jenky/codebuddy/RestClientTest.java
@@ -1,4 +1,3 @@
-/*
 package jenky.codebuddy;
 
 import org.hibernate.validator.constraints.URL;
@@ -9,32 +8,19 @@ import java.io.IOException;
 import java.net.HttpURLConnection;
 
 public class RestClientTest {
-    RestClient noCredentialsClient = new RestClient("","");
-    RestClient wrongCredentialsClient = new RestClient("foo","bar");
-    RestClient sonarqubeCredentialsClient = new RestClient("admin","CC4c96d");
-    RestClient githubCredentialsClient = new RestClient("CodebuddyTest","dekatkrabtdekrullenvandetrap");
     public static void main(String[] args) {
         RestClientTest t = new RestClientTest();
         try{
-            t.testGet();
+            t.bla();
         }
         catch(Exception e){
 
         }
     }
 
-
     @Test
-    public void testGet() throws Exception {
-        assertEquals(200, noCredentialsClient.getStatus());
-        assertEquals(200, wrongCredentialsClient.getStatus());
-        assertEquals(200, sonarqubeCredentialsClient.getStatus());
-        assertEquals(200, githubCredentialsClient.getStatus());
-    }
-
-    @Test
-    public void testPost() throws Exception {
-
+    public void bla() throws Exception {
+        assertEquals(1,1);
     }
 }
-*/
+
