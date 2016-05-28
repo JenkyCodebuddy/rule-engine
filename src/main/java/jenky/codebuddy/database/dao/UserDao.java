9a1b4abc32a0a66769d592508a0fbb88e1bc9ce9
Falio <f.fremouw@gmail.com>
2016-05-28 21:52:52 +0200

    oja vorige classes weer toegevoegd

---
+++ src/main/java/jenky/codebuddy/database/dao/UserDao.java
@@ -0,0 +1,16 @@
+package jenky.codebuddy.database.dao;
+
+import jenky.codebuddy.models.entities.User;
+
+import javax.persistence.Column;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * Created by joost on 26-5-2016.
+ */
+public interface UserDao {
+
+    public List<User> selectAll();
+
+}
