e34912418f657e2d40aa65fcc04afae1291ec7a2
Falio <f.fremouw@gmail.com>
2016-05-29 19:52:52 +0200

    meer comments

--- src/main/java/jenky/codebuddy/database/dao/GenericDataAccessObject.java
+++ src/main/java/jenky/codebuddy/database/dao/GenericDataAccessObject.java
@@ -18,53 +18,91 @@ import java.util.List;
 import java.util.Optional;
 
 /**
- * Created by joost on 24-5-2016.
+ * This class is managed by Spring en gets a SessionFactory injected.
+ * This class is responsible for executing the requested actions on the database.
+ * @param <T>
+ * @param <Id>
  */
 @Repository
 public class GenericDataAccessObject<T, Id extends Serializable> implements DataAccessObjectInterface<T, Id> {
     private Class<T> type;
 
+    /**
+     * injected by Spring
+     */
     @Autowired
     private SessionFactory sessionFactory;
+
+    public GenericDataAccessObject() {
+    }
 
     public GenericDataAccessObject(Class<T> type) {
     }
 
+    /**
+     *
+     * @return type of class
+     */
     public Class<T> getType() {
         return type;
     }
 
+    /**
+     *
+     * @param type
+     */
     public void setType(Class<T> type) {
         this.type = type;
     }
 
-    @Transactional
+    /**
+     * saves the entity in the database
+     * @param entity
+     */
     public void persist(T entity) {
         sessionFactory.getCurrentSession().save(entity);
     }
 
-    @Transactional
+    /**
+     * updates the entity
+     * @param entity
+     */
     public void update(T entity) {
         sessionFactory.getCurrentSession().update(entity);
     }
 
-    @Transactional
+    /**
+     *
+     * @param id
+     * @return entity with matching id
+     */
     public T findById(final int id) {
         return (T)  sessionFactory.getCurrentSession().get(type, id);
     }
 
-    @Transactional
+    /**
+     * deletes the entity from the database
+     * @param entity
+     */
     public void delete(T entity) {
         sessionFactory.getCurrentSession().delete(entity);
     }
 
-    @Transactional
+    /**
+     *
+     * @return all the matching entities of the same type
+     */
     public List<T> findAll() {
         Criteria crit =  sessionFactory.getCurrentSession().createCriteria(type);
         return crit.list();
     }
 
-    @Transactional
+    /**
+     *
+     * @param column
+     * @param value
+     * @return the entity if it exists
+     */
     public T getRecordIfExists(String column, T value){
         Criteria crit =  sessionFactory.getCurrentSession().createCriteria(type)
                 .add(Restrictions.eq(column,value));
@@ -72,7 +110,12 @@ public class GenericDataAccessObject<T, Id extends Serializable> implements Data
         return (T) cr.get().uniqueResult();
     }
 
-    @Transactional
+    /**
+     *
+     * @param column
+     * @param value
+     * @return
+     */
     public boolean checkIfRecordExists(String column, T value){
         Criteria crit =  sessionFactory.getCurrentSession().createCriteria(type)
                 .add(Restrictions.eq(column,value));
