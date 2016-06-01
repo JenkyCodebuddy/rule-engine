980b7ac59ae09bd8bac7072e89146be3af29922f
Falio <f.fremouw@gmail.com>
2016-06-01 19:29:05 +0200

    ok boys

--- src/main/java/jenky/codebuddy/controllers/MainController.java
+++ src/main/java/jenky/codebuddy/controllers/MainController.java
@@ -11,6 +11,7 @@ import jenky.codebuddy.token.Verification;
 import jenky.codebuddy.token.models.Token;
 import org.springframework.web.bind.annotation.*;
 
+import javax.mail.MessagingException;
 import java.security.Key;
 import java.util.Map;
 
@@ -20,23 +21,50 @@ public class MainController {
     String sonarqubeResponse;
     Map<String,String> githubInfoMap;
     private CompleteResult result;
+    //private BusinessLogicController businessLogicController;
+    //private BusinessLogicDB businessLogicDB;
+
+    public MainController(){
+        setBusinessLogicDB(new BusinessLogicDB());
+        setBusinessLogicController(new BusinessLogicController());
+    }
     private Token token = new Token();
     private BusinessLogicController businessLogicController = new BusinessLogicController();
     private BusinessLogicDB businessLogicDB = new BusinessLogicDB();
 
 
     @RequestMapping(value = "/score", method = RequestMethod.POST)
-    public void createScoreFromMetrics(@RequestHeader Map<String,String> headers) { //create new completeResultModel on POST request
-        sonarqubeResponse = headers.get("sonarquberesponse");
-        githubInfoMap = businessLogicController.createGithubUserInfoMap(headers);
-        setCompleteResultModel(new CompleteResultModelBuilder(sonarqubeResponse, githubInfoMap).buildCompleteResultModel());
-        businessLogicDB.storeCompleteResultModel(getCompleteResultModel());
+    public String createScoreFromMetrics(@RequestHeader Map<String,String> headers) { //create new completeResultModel on POST request
+        sonarqubeResponse = headers.get("sonarqubeResponse");
+        return sonarqubeResponse;
+        //githubInfoMap = getBusinessLogicController().createGithubUserInfoMap(headers);
+        //setCompleteResultModel(new CompleteResultModelBuilder(sonarqubeResponse, githubInfoMap).buildCompleteResultModel());
+        //getBusinessLogicDB().storeCompleteResultModel(getCompleteResultModel());
     }
 
     //TODO if statement so that only after the user is authenticated this can be accessed
     @RequestMapping(value = "/score", method = RequestMethod.GET)
-    private CompleteResult getCompleteResultModel(){
-        return result;
+    private void returnModel(){
+        getCompleteResultModel();
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    private void login(@RequestParam String email, String password ){
+       // getBusinessLogicDB().login(email,password);
+    }
+
+    @RequestMapping(value = "/signup", method = RequestMethod.POST)
+    private void signUp(@RequestParam String email, String password){
+        //getBusinessLogicDB().signup(email, password);
+    }
+
+   /* @RequestMapping(value = "/verify", method = RequestMethod.POST){
+
+    }*/
+
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
+    private String test(){
+        return "WE BACK ONLINE BOIS";
     }
 
     //TODO combine this with login
@@ -68,6 +96,28 @@ public class MainController {
         this.result = result;
     }
 
+
+    private CompleteResult getCompleteResultModel(){
+        return result;
+    }
+
+    public BusinessLogicController getBusinessLogicController() {
+        return businessLogicController;
+    }
+
+    public void setBusinessLogicController(BusinessLogicController businessLogicController) {
+        this.businessLogicController = businessLogicController;
+    }
+
+    public BusinessLogicDB getBusinessLogicDB() {
+        return businessLogicDB;
+    }
+
+    public void setBusinessLogicDB(BusinessLogicDB businessLogicDB) {
+        this.businessLogicDB = businessLogicDB;
+    }
+
+    //all requests to the "/score" endpoint
 
     //all requests to the "/score" endpoint
 
