6f49a76d483e46e8a565ceb4ba026f18f346b79d
Falio <f.fremouw@gmail.com>
2016-06-01 16:22:25 +0200

    we dem boys

--- src/main/java/jenky/codebuddy/controllers/MainController.java
+++ src/main/java/jenky/codebuddy/controllers/MainController.java
@@ -4,8 +4,6 @@ import jenky.codebuddy.BusinessLogicController;
 import jenky.codebuddy.BusinessLogicDB;
 import jenky.codebuddy.models.rest.CompleteResult;
 import jenky.codebuddy.modelbuilders.CompleteResultModelBuilder;
-import jenky.codebuddy.models.rest.Mail;
-import jenky.codebuddy.signUpMail;
 import org.springframework.web.bind.annotation.*;
 
 import javax.mail.MessagingException;
@@ -33,14 +31,19 @@ public class MainController {
         return result;
     }
 
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
+    private String test(){
+        return "WE BACK ONLINE BOIS";
+    }
+
     @RequestMapping(value = "/signup", method = RequestMethod.POST)
     private void signUp(@RequestParam String reciepentAddress){
-        signUpMail m  = new signUpMail();
-        try {
-            m.sendMail(reciepentAddress);
-        } catch (MessagingException e) {
-            e.printStackTrace();
-        }
+//        signUpMail m  = new signUpMail();
+//        try {
+//            m.sendMail(reciepentAddress);
+//        } catch (MessagingException e) {
+//            e.printStackTrace();
+//        }
     }
 
     private void setCompleteResultModel(CompleteResult result){
