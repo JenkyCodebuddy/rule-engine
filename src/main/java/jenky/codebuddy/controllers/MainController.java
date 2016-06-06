aaa50f2a4c92d2e08b4e2117d1c4d06506bc2c64
Falio <f.fremouw@gmail.com>
2016-06-06 23:26:58 +0200

    spring xml aangepast maar werkt nog steeds niet

--- src/main/java/jenky/codebuddy/controllers/MainController.java
+++ src/main/java/jenky/codebuddy/controllers/MainController.java
@@ -43,74 +43,74 @@ public class MainController {
         setSignUpService(new SignUpService());
     }
 
-    @RequestMapping(value = "/achievements", method = RequestMethod.GET)
-    private Achievements getAllAchievements(@RequestHeader Map<String,String> headers){
-        if(AuthenticationService.checkIfTokenIsValid(headers.get("token"))){
-            return achievementsService.getAllAchievements();
-        }
-        else{
-            return new Achievements("Token not valid");
-        }
-    }
-
-    @RequestMapping(value = "/login", method = RequestMethod.POST) //
-    private Response login(@RequestHeader Map<String,String> headers) {
-        return loginService.login(headers.get("email"), headers.get("password"));
-    }
-
-
-    @RequestMapping(value = "/profile", method = RequestMethod.POST)
-    private Profile getProfile(@RequestHeader Map<String,String> headers) {
-        if(AuthenticationService.checkIfTokenIsValid(headers.get("token"))){
-            return profileService.returnProfile(headers.get("token"));
-        }
-        else{
-            return new Profile("Token not valid");
-        }
-    }
-
-    @RequestMapping(value = "/score", method = RequestMethod.POST)
-    private void saveScore(@RequestHeader Map<String, String> headers){
-        ScoreUserService scoreUserService = new ScoreUserService();
-        Gson gson = new Gson();
-        String sonarqubeResponse = headers.get("sonarquberesponse");
-        Type test = new TypeToken<List<SonarResponse>>(){}.getType();
-        List<SonarResponse> sonarResponseList = gson.fromJson(sonarqubeResponse, test);
-        SonarResponse sonarResponse = sonarResponseList.get(0);
-        githubInfoMap = scoreUserService.createGithubUserInfoMap(headers);
-        CommitModelBuilder commitModelBuilder = new CommitModelBuilder(githubInfoMap);
-
-        ScoreModelBuilder scoreModelBuilder = new ScoreModelBuilder(sonarResponse, commitModelBuilder.getUserCommitModel());
-        scoreUserService = new ScoreUserService(scoreModelBuilder.getScoreModel(), sonarResponse, commitModelBuilder.getUserCommitModel());
-    }
-
-    @RequestMapping(value = "/shop", method = RequestMethod.GET)
-    private Items getAllItems(@RequestHeader Map<String,String> headers){
-        if(AuthenticationService.checkIfTokenIsValid(headers.get("token"))){
-            return getShopService().getAllItems();
-        }
-        else{
-            return new Items("Token not valid");
-        }
-    }
-
-    @RequestMapping(value = "/signup", method = RequestMethod.POST)
-    public @ResponseBody
-    Response signUpNewUser(@RequestHeader Map<String,String> headers){ //sign up a new user with an email supplied by user
-        return getSignUpService().signUpNewUser(headers.get("email"));
-    }
-
-    @RequestMapping(value = "/verify", method = RequestMethod.POST)
-    public @ResponseBody Response checkVerificationCode(@RequestHeader Map<String, String> headers){
-        return getSignUpService().checkVerificationCode(headers.get("verificationcode"), headers.get("password"));  //check verification code supplied by user, and if correct set password
-    }
-
-
-
-    @RequestMapping(value = "/test", method = RequestMethod.GET)
-    private String test(){
-        return "WE BACK ONLINE BOIS";
-    }
+//    @RequestMapping(value = "/achievements", method = RequestMethod.GET)
+//    private Achievements getAllAchievements(@RequestHeader Map<String,String> headers){
+//        if(AuthenticationService.checkIfTokenIsValid(headers.get("token"))){
+//            return achievementsService.getAllAchievements();
+//        }
+//        else{
+//            return new Achievements("Token not valid");
+//        }
+//    }
+//
+//    @RequestMapping(value = "/login", method = RequestMethod.POST) //
+//    private Response login(@RequestHeader Map<String,String> headers) {
+//        return loginService.login(headers.get("email"), headers.get("password"));
+//    }
+//
+//
+//    @RequestMapping(value = "/profile", method = RequestMethod.POST)
+//    private Profile getProfile(@RequestHeader Map<String,String> headers) {
+//        if(AuthenticationService.checkIfTokenIsValid(headers.get("token"))){
+//            return profileService.returnProfile(headers.get("token"));
+//        }
+//        else{
+//            return new Profile("Token not valid");
+//        }
+//    }
+//
+//    @RequestMapping(value = "/score", method = RequestMethod.POST)
+//    private void saveScore(@RequestHeader Map<String, String> headers){
+//        ScoreUserService scoreUserService = new ScoreUserService();
+//        Gson gson = new Gson();
+//        String sonarqubeResponse = headers.get("sonarquberesponse");
+//        Type test = new TypeToken<List<SonarResponse>>(){}.getType();
+//        List<SonarResponse> sonarResponseList = gson.fromJson(sonarqubeResponse, test);
+//        SonarResponse sonarResponse = sonarResponseList.get(0);
+//        githubInfoMap = scoreUserService.createGithubUserInfoMap(headers);
+//        CommitModelBuilder commitModelBuilder = new CommitModelBuilder(githubInfoMap);
+//
+//        ScoreModelBuilder scoreModelBuilder = new ScoreModelBuilder(sonarResponse, commitModelBuilder.getUserCommitModel());
+//        scoreUserService = new ScoreUserService(scoreModelBuilder.getScoreModel(), sonarResponse, commitModelBuilder.getUserCommitModel());
+//    }
+//
+//    @RequestMapping(value = "/shop", method = RequestMethod.GET)
+//    private Items getAllItems(@RequestHeader Map<String,String> headers){
+//        if(AuthenticationService.checkIfTokenIsValid(headers.get("token"))){
+//            return getShopService().getAllItems();
+//        }
+//        else{
+//            return new Items("Token not valid");
+//        }
+//    }
+//
+//    @RequestMapping(value = "/signup", method = RequestMethod.POST)
+//    public @ResponseBody
+//    Response signUpNewUser(@RequestHeader Map<String,String> headers){ //sign up a new user with an email supplied by user
+//        return getSignUpService().signUpNewUser(headers.get("email"));
+//    }
+//
+//    @RequestMapping(value = "/verify", method = RequestMethod.POST)
+//    public @ResponseBody Response checkVerificationCode(@RequestHeader Map<String, String> headers){
+//        return getSignUpService().checkVerificationCode(headers.get("verificationcode"), headers.get("password"));  //check verification code supplied by user, and if correct set password
+//    }
+//
+//
+//
+//    @RequestMapping(value = "/test", method = RequestMethod.GET)
+//    private String test(){
+//        return "WE BACK ONLINE BOIS";
+//    }
 
     private void setCompleteResultModel(CompleteResult result){
         this.result = result;
