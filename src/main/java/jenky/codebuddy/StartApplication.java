f1eb030cbb0ee79cf029070222d295fc0f3e6565
Falio <f.fremouw@gmail.com>
2016-06-08 17:22:06 +0200

    test

--- src/main/java/jenky/codebuddy/StartApplication.java
+++ src/main/java/jenky/codebuddy/StartApplication.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.ImportResource;
 @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 @SpringBootApplication
 @ImportResource("spring.xml")
-public class StartApplication { //class for starting the spring application
+public class StartApplication { //class for starting the spring application.
 
 	public static void main(String[] args) {
 		SpringApplication.run(StartApplication.class, args);
