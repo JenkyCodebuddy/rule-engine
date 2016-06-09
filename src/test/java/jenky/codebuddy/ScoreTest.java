02891caef55ad38d9f3f7ba9eb59f294374f749d
Falio <f.fremouw@gmail.com>
2016-06-09 14:44:45 +0200

    first try score commit

--- src/test/java/jenky/codebuddy/ScoreTest.java
+++ src/test/java/jenky/codebuddy/ScoreTest.java
@@ -1,7 +1,12 @@
 package jenky.codebuddy;
 
+import jenky.codebuddy.database.generic.GenericDaoImpl;
+import jenky.codebuddy.models.entities.Score;
 import jenky.codebuddy.models.rest.UserCommit;
+import org.hibernate.SessionFactory;
 import org.junit.Test;
+
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
@@ -9,26 +14,24 @@ import static org.junit.Assert.assertNotEquals;
 /**
  * Created by Fabian on 9-6-2016.
  */
-public class ScoreTest {
-    public static void main(String[] args) {
-        RestClientTest t = new RestClientTest();
-        try{
-            t.bla();
-            t.testModel();
-        }
-        catch(Exception e){
+public class ScoreTest extends GenericDaoImpl<Score, Integer> {
 
-        }
+
+    public static void main(String[] args) {
+
     }
 
     @Test
-    public void bla() throws Exception {
-        assertEquals(1,1);
+    public void getScore() throws Exception {
+        Score testScore = new Score();
+        Score score = super.findById(1);
+        assertEquals(testScore, score);
     }
 
     @Test
     public void testModel() throws Exception{
-        UserCommit c = new UserCommit("Joost","joost1235@hotmail.com","master","asdasd12113ui1h3ir","cyka/TestclassForDAO/idi/nahui");
-        assertNotEquals(c.getEmail(),"");
+//        UserCommit c = new UserCommit("Joost","joost1235@hotmail.com","master","asdasd12113ui1h3ir","cyka/TestclassForDAO/idi/nahui");
+//        assertNotEquals(c.getEmail(),"");
     }
+
 }
