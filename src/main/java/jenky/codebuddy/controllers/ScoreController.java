77e5a544eb9c77fd1dc2f80732adde8d79db24d8
Falio <f.fremouw@gmail.com>
2016-06-09 21:12:43 +0200

    gefixed
    let op unit tests zijn uitgeschakeld!

--- src/main/java/jenky/codebuddy/controllers/ScoreController.java
+++ src/main/java/jenky/codebuddy/controllers/ScoreController.java
@@ -40,7 +40,6 @@ public class ScoreController {
         Type sonar = new TypeToken<List<SonarResponse>>(){}.getType();
         List<SonarResponse> sonarResponseList = gson.fromJson(sonarqubeResponse.replaceAll("\\s",""), sonar);
         SonarResponse sonarResponse = sonarResponseList.get(0);
-        System.out.println("headers = " + headers);
         githubInfoMap = scoreUserService.createGithubUserInfoMap(headers);
         CommitModelBuilder commitModelBuilder = new CommitModelBuilder(githubInfoMap);
 
