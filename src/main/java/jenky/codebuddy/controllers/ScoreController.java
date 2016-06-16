2766b647bb00a07c789d4fe5c3d42089d99bd650
Falio <f.fremouw@gmail.com>
2016-06-16 17:18:01 +0200

    1 method

--- src/main/java/jenky/codebuddy/controllers/ScoreController.java
+++ src/main/java/jenky/codebuddy/controllers/ScoreController.java
@@ -2,7 +2,10 @@ package jenky.codebuddy.controllers;
 
 import jenky.codebuddy.services.ScoreUserService;
 import jenky.codebuddy.services.ScoreUserServiceImpl;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
@@ -11,7 +14,7 @@ import java.util.Map;
  * to post the sonar results and commiter information to be saved in the database
  */
 @RestController
-@RequestMapping(value = "/save")
+@RequestMapping(value = "/score")
 public class ScoreController {
 
     /**
@@ -19,8 +22,8 @@ public class ScoreController {
      * @param headers must contains a sonarresponse(json)
      * and header email, header sha, header branch, header project, username
      */
-    @RequestMapping(value = "/score", method = RequestMethod.POST)
-    private void saveCommit(@RequestHeader Map<String, String> headers){
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    private void saveScore(@RequestHeader Map<String, String> headers){
         ScoreUserService scoreUserService = new ScoreUserServiceImpl();
         scoreUserService.parseHeaders(headers);
     }
