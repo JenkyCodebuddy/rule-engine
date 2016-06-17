0dc7ccc9c664467fe44a86fe72354cc2d46a5dd3
Joost de Ruijter <joost1235@hotmail.com>
2016-06-17 20:38:48 +0200

    added /messaging/notifications and /messaging/logout endpoints for setting and removing a messagingtoken for push notifications

---
+++ src/main/java/jenky/codebuddy/controllers/MessagingController.java
@@ -0,0 +1,60 @@
+package jenky.codebuddy.controllers;
+
+/**
+ * Created by joost on 17-6-2016.
+ */
+
+import jenky.codebuddy.models.rest.Response;
+import jenky.codebuddy.services.AuthenticationService;
+import jenky.codebuddy.services.MessagingService;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+import java.util.Optional;
+
+/**
+ * This endpoint is used for google cloud messaging.
+ */
+@RestController
+@RequestMapping(value = "/messaging")
+public class MessagingController {
+
+    MessagingService messagingService;
+
+    public MessagingController() {
+        this.messagingService = new MessagingService();
+    }
+
+    /**
+     * @param headers Contains the token of the user.
+     * @return achievements or "Token is invalid"
+     */
+    @RequestMapping(value = "/notifications", method = RequestMethod.POST)
+    private Response saveMessagingTokenForUser(@RequestHeader Map<String,String> headers){
+        Optional result = Optional.ofNullable(headers.get("token"));
+        if(result.isPresent()) {
+            if (AuthenticationService.checkIfTokenIsValid(headers.get("token"))) {
+                return messagingService.saveMessagingTokenForUser(headers.get("token"), headers.get("messagingtoken"));
+            } else {
+                return new Response(400, "Token not valid");
+            }
+        }
+        else{
+            return new Response(200,"Did nothing");
+        }
+    }
+
+    @RequestMapping(value = "/logout", method = RequestMethod.POST)
+    private Response deleteMessagingTokenForUser(@RequestHeader Map<String,String> headers){
+        if(AuthenticationService.checkIfTokenIsValid(headers.get("token"))){
+            return messagingService.deleteMessagingTokenForUser(headers.get("token"), headers.get("messagingtoken"));
+        }
+        else{
+            return new Response(400,"Token not valid");
+        }
+    }
+}
+
--- src/main/java/jenky/codebuddy/services/CloudMessagingService.java
+++
@@ -1,7 +0,0 @@
-package jenky.codebuddy.services;
-
-/**
- * Created by joost on 17-6-2016.
- */
-public class CloudMessagingService {
-}
