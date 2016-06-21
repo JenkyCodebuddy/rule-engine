17e930e8972c6c75896610e87bc383abd563d5fa
Falio <f.fremouw@gmail.com>
2016-06-21 13:58:50 +0200

    made final String

--- src/main/java/jenky/codebuddy/services/ScoreUserServiceImpl.java
+++ src/main/java/jenky/codebuddy/services/ScoreUserServiceImpl.java
@@ -23,7 +23,8 @@ public class ScoreUserServiceImpl implements ScoreUserService {
 
     private User user;
     private MessagingService messagingService;
-
+    private final String successColour = "#1472ff";
+    private final String failColour = "#ff0000";
     public ScoreUserServiceImpl() {
 
     }
@@ -46,9 +47,9 @@ public class ScoreUserServiceImpl implements ScoreUserService {
             SonarResponse sonarResponse = sonarResponseList.get(0);
             ScoreModelBuilder scoreModelBuilder = new ScoreModelBuilder(sonarResponse, userCommit);
             saveUserScore(scoreModelBuilder.getScoreModel(), sonarResponse, userCommit);
-            this.messagingService.sendPush("Results are in, check your profile!", "#ff1414", messageId);
+            this.messagingService.sendPush("Results are in, check your profile!", successColour, messageId);
         } else {
-            this.messagingService.sendPush("uhoh you broke the build! No scores earned!", "#ff0000", messageId);
+            this.messagingService.sendPush("uhoh you broke the build! No scores earned!", failColour, messageId);
         }
     }
 
@@ -200,7 +201,7 @@ public class ScoreUserServiceImpl implements ScoreUserService {
                 if(userWithBestScoreForMetric != null){
                     this.messagingService.sendPush(
                             "If you want to improve the following metric: \" + metric + \", ask \" + userWithBestScoreForMetric.getEmail() + \"! He/she has the best score",
-                            "#ff1414",
+                            successColour,
                             messageId);
                 }
                 else{
